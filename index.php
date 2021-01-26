91964e6a335b3bbb64b7dd4e35dade8c6f717f15
benz830 <77878804+benz830@users.noreply.github.com>
2021-01-26 12:38:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
 <font color="white"><h1><b><font face="arial"><p align="center"> TODAY </b></h1>
 <p align="center"><font size="80"><u><b> "THE STORY OF POVERTY" </p allign"center"> </u> </b> </font size="80"
 </p>
+  <div class="row">
+  <div class="column">
+    <img src="poberty.png" alt="Snow" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="3969.jpg" alt="Forest" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="download.jfif" alt="Mountains" style="width:100%">
+  </div>
+</div>
 <p align="center"><img width="500px" img src="3969.jpg"></p align="center">
 <br>
 <br>
