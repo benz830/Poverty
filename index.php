cd2e135514dc169e65b32b11efd9f8e9fbbb3aee
benz830 <77878804+benz830@users.noreply.github.com>
2021-01-26 13:01:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,39 @@
 <body background="bg.jpg">
 <font color="white"><h1><b><font face="arial"><p align="center"> TODAY </b></h1>
 <p align="center"><font size="80"><u><b> "THE STORY OF POVERTY" </p allign"center"> </u> </b> </font size="80"
-</p>
+</p><div style='text-align:center;font-weight:bold'>
+		
+    <h1>
+      Just couple of reasons we go to France every year......
+    </h1>
+		
+    <table class="image" align="center" >
+    <tr>
+      <td>
+        <img src="3969.jpg" 
+               height="325" width="500" border="4"
+               alt="Sardines, Bordeaux"  
+        />
+      </td>
+      <td>
+        <img src="poberty.png" 
+               height="325" width="500" border="4"
+               alt="Mid-day in Paris"  
+        />
+      </td>
+    </tr>
+    <tr>
+      <td class="caption">
+        Sardines in Bordeaux
+      </td>
+      <td class="caption">
+        Lunch in Paris
+      </td>
+    </tr>
+    </table>
+  </div>
+
+
 <p align="center"><img width="450px" img src="3969.jpg"></p align="center">
 <br>
 <br>
