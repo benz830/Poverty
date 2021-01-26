64b5be2c9a2ed77665f426a3c7a856dfb3c9d3cd
benz830 <77878804+benz830@users.noreply.github.com>
2021-01-26 12:28:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <font color="white"><h1><b><font face="arial"><p align="center"> TODAY </b></h1>
 <p align="center"><font size="80"><u><b> "THE STORY OF POVERTY" </p allign"center"> </u> </b> </font size="80"
 </p>
-<p align="center"><img width="700px" img src="3969.jpg"></p align="center">
+<p align="center"><img width="500px" img src="3969.jpg"></p align="center">
 <br>
 <br>
 <br>
