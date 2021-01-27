875db7c6905f1893fda4a5ee98dea878a2cf49d7
benz830 <77878804+benz830@users.noreply.github.com>
2021-01-27 21:04:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <title> POVERTY </title>
 </head>
 <body>
-<body background="bkg.png">
-<font color="white"><h1><b><font face="arial"><p align="center"> TODAY </b></h1>
+<body background="background.jpg">
+	<br>
+	<br>
+	<br>
+<font color="blue"><h1><b><font face="arial"><p align="center"> TODAY </b></h1>
 <p align="center"><font size="80"><u><b> "THE STORY OF POVERTY" </p allign"center"> </u> </b> </font size="80"
 </p><div style='text-align:center;font-weight:bold'>
 				
