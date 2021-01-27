8ea6db9b8ced110f69463b1783b089a07191a2cc
benz830 <77878804+benz830@users.noreply.github.com>
2021-01-27 21:12:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 <br>
 <br>
 <br>
-<br><center><b><h3><font color="blue"> Group 1
+<br><center><b><h2><font color="lime"> Group 1
 <br>
 <br>
 Villanueva, Jasmine Rin
@@ -55,6 +55,6 @@ Martinez, Benz
 Chua, Aldrin
 <br>
 <br>
-Lumuag, Leo </center></b></h3></font>
+Lumuag, Leo </center></b></h2></font>
 </body>
 </html>
